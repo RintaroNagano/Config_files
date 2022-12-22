af5c682ecbf175add688e2a22630f923293bc69b
RintaroNagano <rintaro.nagano@gmail.com>
2022-12-22 18:40:35 +0900

    [update] .bashrc add ll and la alies

--- setup/setup_bash.sh
+++ setup/setup_bash.sh
@@ -10,3 +10,5 @@ echo "GIT_PS1_SHOWDIRTYSTATE=true"
 echo "export PS1='\[\033[32m\]\u@\h\[\033[00m\]:\[\033[34m\]\w\[\033[31m\]\$(__git_ps1)\[\033[00m\]\$ '" 
 } >> .bashrc
 rm setup_bash.sh
+echo "alias ll='ls -al --color=auto'" >> .bashrc
+echo "alias la='ls -a --color=auto'" >> .bashrc
